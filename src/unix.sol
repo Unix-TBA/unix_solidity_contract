cf5996f35f1ee73bc57a4860a7f99b35d3181eb6
Adebara123 <adebaraayomide@gmail.com>
2024-07-11 12:46:20 +0100

    Chore: unix initiated

---
+++ src/unix.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyToken is ERC721, ERC721URIStorage {
+    constructor() ERC721("MyToken", "MTK") {}
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
